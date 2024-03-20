13956c56181d371939bc0cd8e2348ad6e8a7606d
UF-Swamp-Launch-Rocket-Team <nasasl.ufrocketteam@gmail.com>
2024-03-20 18:58:24 -0400

    Add main makefile, edited sensor_test

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-default:
-	echo "NOT DONE YET"
+default:main.cpp IMU/imu.cpp
+	c++ -std=c++11 -g main.cpp IMU/imu.cpp Dynamics_Model_Controller/controller.cpp Dynamics_Model_Controller/dynamics_model.cpp  Dynamics_Model_Controller/drag.cpp Dynamics_Model_Controller/pi.cpp busynano/busynano.cpp bitbang/bitbang.cpp -o main -lwiringPi -O3
 
-sensor_test:
+sensor_test: sensor_test.cpp IMU/imu.cpp bitbang/bitbang.cpp busynano/busynano.cpp
 	c++ -std=c++11 -g sensor_test.cpp IMU/imu.cpp bitbang/bitbang.cpp busynano/busynano.cpp -o sensor_test -lwiringPi
--- sensor_test.cpp
+++ sensor_test.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include "Altimiter/altimiter.h"
 #include "IMU/imu.h"
+#include "fstream"
 
 #include "busynano/busynano.h"
 #include "bitbang/bitbang.h"
@@ -30,8 +31,16 @@ int main()
 
     imu_init();
 
-    while (true)
+    std::ofstream sensor_testing;
+    sensor_testing.open("Sensor Testing");
+
+    auto start = chrono::high_resolution_clock::now();
+
+    int ii = 0;
+
+    while (ii < 10000)
     {
+        int a = 0;
         auto cur = chrono::high_resolution_clock::now();
 
         altimiter_t alt_data = get_temp_and_pressure();
@@ -39,13 +48,21 @@ int main()
         imu_data_t imu_data = imu_read_data();
 
         auto t_sample = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - cur).count();
-        std::cout << "Time to sample:\t" << std::endl;
+        // std::cout << "Time to sample:\t" << t_sample << std::endl;
 
-        print_all(&imu_data);
+        // print_all(&imu_data);
 
-        float alt = Altitude_calc(alt_data.pressure);
-        std::cout << "TEMP: " + std::to_string(alt_data.temp) << "\tPRESS: " + std::to_string(alt_data.pressure) << "\tALT: "  + std::to_string(alt) << std::endl;
+        // float alt = Altitude_calc(alt_data.pressure);
+        // std::cout << "TEMP: " + std::to_string(alt_data.temp) << "\tPRESS: " + std::to_string(alt_data.pressure) << "\tALT: "  + std::to_string(alt) << std::endl;
 
+        auto t_elaps = chrono::duration<double>(chrono::high_resolution_clock::now() - start).count();
+        // std::cout << "Total Elasped Time:\t" << t_elaps << "\t" << "seconds" << std::endl;
+        // sensor_testing <<"%d, %d,%d,%d,%d,%d,%d,%d,%d,%d,%d", cur, alt_data.temp, alt_data.pressure, imu_data.heading.x, imu_data.heading.y, imu_data.heading.z, imu_data.accel.x, imu_data.accel.y, imu_data.accel.z, t_elaps << std::endl;;
+        sensor_testing << chrono::duration<double>(chrono::high_resolution_clock::now() - start).count() << "\t" << alt_data.pressure << "\t" << alt_data.temp << "\t" << imu_data.heading.x << "\t" << imu_data.heading.y << "\t" << imu_data.heading.z << "\t" << imu_data.accel.x << "\t" << imu_data.accel.y << "\t" << imu_data.accel.z << std::endl;
         usleep(10000);
+        ii += 1;
     }
+
+    sensor_testing.close();
+    return 0;
 }
