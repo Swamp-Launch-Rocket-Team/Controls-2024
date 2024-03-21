06c467448681cee6656234da8a0f63bb93166c50
UF-Swamp-Launch-Rocket-Team <nasasl.ufrocketteam@gmail.com>
2024-03-21 00:27:33 -0400

    Add dummy file for testing the run on boot

---
+++ dummymake.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    // ofstream file;
+    // file.open("/home/airbrake/Controls-2024/test.fuck");
+    // file << "The mitochondria is the powerhouse of the cell" << endl;
+    int i = 1;
+    while (true)
+    {
+        i += 1;
+    }
+    
+    // std::cout << "This worked" << std::endl;
+    return 0;
+}
