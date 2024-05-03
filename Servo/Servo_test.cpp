8cd9d5171bc2b93cc2782aa6935889e51ba34beb
UF-Swamp-Launch-Rocket-Team <nasasl.ufrocketteam@gmail.com>
2024-05-03 01:17:33 -0400

    Servo test updated. New servo was tested succesfully.

--- Servo/Servo_test.cpp
+++ Servo/Servo_test.cpp
@@ -29,7 +29,7 @@ int main()
     //Finding the pwm signals that correspond to zero and 180
     //Remember the servo rotates counter clockwise for our application
     //For 0: start at 345
-    //pwmWrite(Pwm_pin, 345);
+    // pwmWrite(Pwm_pin, 625);
     
     //For 180: start at 625 and see if this corresponds to 105 degrees of rotation
     //pwmWrite(Pwm_pin, 625);
@@ -38,14 +38,15 @@ int main()
 
 
 
-    // //Slope and intercept for servo angle to PWM signal mapping
-    // float m = (480.0/180.0);        //THIS IS THE ACTUAL SLOPE WE USE
-    // float b = 345;                  //THIS IS THE B WE USE, THIS IS ALSO THE PWM_HOME VALUE AKA 0 DEGREE EXTENSION
+    //Slope and intercept for servo angle to PWM signal mapping
+    float m = (480.0/180.0);        //THIS IS THE ACTUAL SLOPE WE USE
+    float b = 345;                  //THIS IS THE B WE USE, THIS IS ALSO THE PWM_HOME VALUE AKA 0 DEGREE EXTENSION
 
     // //Use this section to write the servo to a specified angle, this is after verifying the slope m and intercept b
-    // float ext = round(m*0 + b);
+    // float ext = round(m*105 + b);
     // pwmWrite(Pwm_pin, ext);
     // delay(500);
+    // pwmWrite(Pwm_pin, b);
 
 
 
@@ -53,19 +54,19 @@ int main()
 
 
     //Aibrake flap extension test from 0 degrees to a specified value in the int ext definition line.
-    // pwmWrite(Pwm_pin, b);
-    // delay(500);
-    // delay(1000);
-    // int ext = round(m*90 + b);
+    pwmWrite(Pwm_pin, b);
+    delay(500);
+    delay(1000);
+    float ext = round(m*105 + b);
 
-    // for(int i = 0; i < 5; i++)
-    // {
-    //     pwmWrite(Pwm_pin, b);
-    //     delay(1000);
-    //     pwmWrite(Pwm_pin, ext);
-    //     delay(1000);
-    // }
-    //pwmWrite(Pwm_pin, b);
+    for(int i = 0; i < 5; i++)
+    {
+        pwmWrite(Pwm_pin, b);
+        delay(1000);
+        pwmWrite(Pwm_pin, ext);
+        delay(1000);
+    }
+    pwmWrite(Pwm_pin, b);
 
 
 
@@ -74,8 +75,8 @@ int main()
     //Aibrake flap extension test from 0 degrees to 105 degrees and then from 105 to 0 degrees
     // pwmWrite(Pwm_pin, b);
     // delay(500);
-    //Use m and b to create a mapping from 0 degrees to 105 degrees.
-    // int pwm_command;
+    // // Use m and b to create a mapping from 0 degrees to 105 degrees.
+    // float pwm_command;
     // for(int i = 0; i < 106; i = i + 5)
     // {
     //     pwm_command = round(m*i + b);
