883c6227b3d6bbad56051cc37ab83e1c85f7689e
UF-Swamp-Launch-Rocket-Team <nasasl.ufrocketteam@gmail.com>
2024-06-12 22:57:37 -0400

    Add theta calc to sensor test

--- sensor_test.cpp
+++ sensor_test.cpp
@@ -37,8 +37,9 @@ int main()
     auto start = chrono::high_resolution_clock::now();
 
     int ii = 0;
+    float theta = 0.0;
 
-    while (ii < 400)
+    while (ii < 800)
     {
         int a = 0;
         auto cur = chrono::high_resolution_clock::now();
@@ -46,7 +47,9 @@ int main()
         altimiter_t alt_data = get_temp_and_pressure();
 
         imu_data_t imu_data = imu_read_data();
-
+        
+        theta = abs(90 - imu_data.heading.y);
+        
         auto t_sample = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - cur).count();
         // std::cout << "Time to sample:\t" << t_sample << std::endl;
 
@@ -58,7 +61,7 @@ int main()
         auto t_elaps = chrono::duration<double>(chrono::high_resolution_clock::now() - start).count();
         // std::cout << "Total Elasped Time:\t" << t_elaps << "\t" << "seconds" << std::endl;
         // sensor_testing <<"%d, %d,%d,%d,%d,%d,%d,%d,%d,%d,%d", cur, alt_data.temp, alt_data.pressure, imu_data.heading.x, imu_data.heading.y, imu_data.heading.z, imu_data.accel.x, imu_data.accel.y, imu_data.accel.z, t_elaps << std::endl;;
-        sensor_testing << chrono::duration<double>(chrono::high_resolution_clock::now() - start).count() << "\t" << alt_data.pressure << "\t" << alt_data.temp << "\t" << imu_data.heading.x << "\t" << imu_data.heading.y << "\t" << imu_data.heading.z << "\t" << imu_data.accel.x << "\t" << imu_data.accel.y << "\t" << imu_data.accel.z << std::endl;
+        sensor_testing << chrono::duration<double>(chrono::high_resolution_clock::now() - start).count() << "\t" << alt_data.pressure << "\t" << alt_data.temp << "\t" << imu_data.heading.x << "\t" << imu_data.heading.y << "\t" << imu_data.heading.z << "\t" << imu_data.accel.x << "\t" << imu_data.accel.y << "\t" << imu_data.accel.z << "\t" << theta << std::endl;
         usleep(10000);
         ii += 1;
     }
